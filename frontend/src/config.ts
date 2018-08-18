d6bd7afd15fac26569996ff8562d7eba36ff7cf6
Neil Ashford <ashfordneil0@gmail.com>
2018-08-18 12:26:29 +1000

    api wrapper for getting a board

---
+++ frontend/src/config.ts
@@ -0,0 +1,5 @@
+import * as axios from "axios";
+
+export const axiosConfig: axios.AxiosRequestConfig = {
+    baseURL: "http://localhost:8080/",
+};
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,10 @@
 import * as React from "react";
 
-console.log("hello world");
+import * as board from "./board/services";
+
+const main = async () => {
+    const x = await board.get("7bbd0911-a374-4ba4-a650-59b843d6fb9a");
+    console.log(x);
+}
+
+main();
