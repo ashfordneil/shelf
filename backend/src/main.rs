5ff08d736ad8f9521c871ee539b5f96eaeab8939
Simon Gordon <gordonsimon@hotmail.com>
2018-08-18 14:31:56 +1000

    Check tiles in and out

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -66,10 +66,41 @@ impl_web! {
             }
         }
 
+        #[get("/tile/:id")]
+        #[content_type("json")]
+        fn get_tile(&self, id: String) -> Result<Tile, ()> {
+            let id = Uuid::parse_str(&id).map_err(|e| {
+                println!("{:?}", e);
+            })?;
+            Tile::get(&id).ok_or(())
+        }
+
         #[post("/tile")]
         #[content_type("json")]
         fn post_tile(&self, body: Tile) -> Result<UuidWrapper, ()> {
             Ok(UuidWrapper(Tile::post(body)))
+        }
+
+        #[post("/tile/:id")]
+        fn checkout_tile(&self, id: String) -> Result<String, ()> {
+            let id = Uuid::parse_str(&id).map_err(|e| {
+                println!("{:?}", e);
+            })?;
+            Tile::checkout(&id).ok_or(())
+        }
+
+        #[patch("/tile/:id")]
+        fn checkin_tile(&self, id: String, jwt: String, body: Tile) -> Result<String, ()> {
+            let id = Uuid::parse_str(&id).map_err(|e| {
+                println!("{:?}", e);
+            })?;
+            let resp = Tile::checkin(&id, jwt, body);
+            if let Ok(_) = resp {
+                Ok("ok".to_string().to_string())
+            }
+            else {
+                Err(())
+            }
         }
     }
 }
--- backend/src/tile.rs
+++ backend/src/tile.rs
@@ -4,13 +4,20 @@ use std::sync::{Arc, Mutex};
 
 use uuid::Uuid;
 
+use jwt::{encode, decode, Header, Algorithm, Validation};
+
 #[derive(Default, Clone, Debug, Response, Extract)]
 pub struct Tile {
     pub content: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct JwtClaims {
+    tile_id: Uuid,
+}
+
 impl Tile {
-    fn storage() -> Arc<Mutex<HashMap<Uuid, Tile>>> {
+    fn tile_storage() -> Arc<Mutex<HashMap<Uuid, Tile>>> {
         lazy_static! {
             static ref STORAGE: Arc<Mutex<HashMap<Uuid, Tile>>> = Default::default();
         }
@@ -18,16 +25,69 @@ impl Tile {
         STORAGE.clone()
     }
 
+    fn auth_storage() -> Arc<Mutex<HashMap<Uuid, String>>> {
+        lazy_static! {
+            static ref STORAGE: Arc<Mutex<HashMap<Uuid, String>>> = Default::default();
+        }
+
+        STORAGE.clone()
+    }
+
     /// Get the internals of a Tile
     pub fn get(id: &Uuid) -> Option<Tile> {
-        let store = Tile::storage();
+        let store = Tile::tile_storage();
         let store = store.lock().unwrap();
         store.get(id).cloned()
     }
 
+    pub fn checkout(tile_id: &Uuid) -> Option<String> {
+        let store = Tile::tile_storage();
+        let store = store.lock().unwrap();
+        if let Some(tile) = store.get(tile_id).cloned() {
+            let authstore = Tile::auth_storage();
+            let mut authstore = authstore.lock().unwrap();
+            if authstore.contains_key(tile_id) {
+                return None;
+            }
+
+            let claims = JwtClaims {
+                tile_id: *tile_id
+            };
+            let new_jwt = encode(&Header::default(), &claims, "secret".as_ref());
+            if let Ok(new_jwt) = new_jwt {
+                authstore.insert(tile_id.clone(), new_jwt.to_string());
+                Some(new_jwt.to_string())
+            }
+            else {
+                None
+            }
+        }
+        else {
+            None
+        }
+    }
+
+    pub fn checkin(tile_id: &Uuid, jwt: String, tile: Tile) -> Result<(), ()> {
+        let authstore = Tile::auth_storage();
+        let stored_jwt = {
+            let authstore = authstore.lock().unwrap();
+            // TODO: Add error checking
+            let entry = authstore.get(tile_id).unwrap().clone();
+            entry
+        };
+        if (jwt.eq(&stored_jwt)) {
+            let mut authstore = authstore.lock().unwrap();
+            authstore.remove(tile_id);
+        }
+        else {
+            return Err(());
+        }
+        Ok(())
+    }
+
     /// Create a new Tile, and return a reference to it.
     pub fn post(new_tile: Tile) -> Uuid {
-        let store = Tile::storage();
+        let store = Tile::tile_storage();
         let mut store = store.lock().unwrap();
         let uuid = loop {
             let uuid = Uuid::new_v4();
